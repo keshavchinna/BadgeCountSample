94aff109cb8f5dd237becec13fc8a59a9c54fe9f
keshavchinna <y.keshav@rocketmail.com>
2014-08-08 01:00:06 +0530

    initial commit

---
+++ src/com/example/BadgeCountSample/MyActivity.java
@@ -0,0 +1,36 @@
+package com.example.BadgeCountSample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+    TextView tv;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        onCreateOptionsMenu(menu, inflater);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+
+        RelativeLayout badgeLayout = (RelativeLayout) menu.findItem(R.id.badge).getActionView();
+        tv = (TextView) badgeLayout.findViewById(R.id.textOne);
+        tv.setText("12");
+    }
+}
