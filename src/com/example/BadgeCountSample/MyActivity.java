a6cc4b62ec027ca9c347a0d35829af913c5c1b81
keshavchinna <y.keshav@rocketmail.com>
2014-08-08 01:26:37 +0530

    applied onClick listener to the counts in the action bar

--- src/com/example/BadgeCountSample/MyActivity.java
+++ src/com/example/BadgeCountSample/MyActivity.java
@@ -2,12 +2,14 @@ package com.example.BadgeCountSample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements View.OnClickListener {
     TextView tv, tv1;
     String counts, sampleCount;
 
@@ -34,6 +36,8 @@ public class MyActivity extends Activity {
         RelativeLayout badgeLayout1 = (RelativeLayout) menu.findItem(R.id.badge1).getActionView();
         tv = (TextView) badgeLayout.findViewById(R.id.textOne);
         tv1 = (TextView) badgeLayout1.findViewById(R.id.textTwo);
+        tv.setOnClickListener(this);
+        tv1.setOnClickListener(this);
         tv.setText(counts);
         tv1.setText(sampleCount);
     }
@@ -41,5 +45,18 @@ public class MyActivity extends Activity {
     public void applyCounts() {
         counts = "25";
         sampleCount = "50";
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.textOne:
+                Log.d("test:", "testOne clicked");
+                break;
+            case R.id.textTwo:
+                Log.d("test:", "testTwo clicked");
+                break;
+        }
     }
 }
